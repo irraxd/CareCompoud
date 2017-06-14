e0da787a1105f59bcc62d7beb278c2513cd2a9e0
irraxd <irraxdgonz7@gmail.com>
2017-06-14 19:29:35 -0400

    Proyecto_CC_V-00
    Versión:
    Inicio de la aplicación.
    
    Desarrollado:
    -Form con la conexión establecida a SQLite

---
+++ CareCompoud_SQLite/CareCompoud_SQLite/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace CareCompoud_SQLite
+{
+        public partial class Form1 : Form
+    {
+        SQLiteConnection conexion;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+              try
+            {
+                conexion = new SQLiteConnection("Data Source= CareCompoud.sqlite");
+                conexion.Open();
+                MessageBox.Show("Conexión Exitosa");
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message,"Error de Conexión");
+            }
+        }
+    }
+}
